bc0e4ca6a92b4d2332920e9f9d6b78fd70b83e6b
yelangya3826850 <yelangya3826850@126.com>
2014-07-26 22:55:13 +0800

    Change
    Change

--- Regression/test.R
+++ Regression/test.R
@@ -23,3 +23,43 @@ summary(lm(Fertility ~ . , data = swiss))
 
 
 
+data(iris)
+library(ggplot2)
+library(caret)
+inTrain <- createDataPartition(y <- iris$Species,
+                               p = 0.7,list = F)
+training <- iris[inTrain,]
+testing <- iris[-inTrain,]
+modFit <- train(Species~.,data=training, method = 'rf',prox = T)
+modFit
+getTree(modFit$finalModel, k =2)
+
+irisP <- classCenter(training[,c(3,4)],training$Species, modFit$finalModel$prox)
+irisP <- as.data.frame(irisP); irisP$Species <- rownames(irisP)
+p <- qplot(Petal.Width, Petal.Length, col = Species, data  =training)
+p + geom_point(aes(x=Petal.Width,y=Petal.Length,col=Species),size=5,shape=4,data=irisP)
+pred <- predict(modFit,testing); testing$predRight <- pred==testing$Species
+table(pred,testing$Species)
+qplot(Petal.Width,Petal.Length,colour = predRight,data = testing,main="newdata Predictions")
+
+
+
+test = iris[ c(1:10, 51:60, 101:110), ]
+train = iris[ c(11:50, 61:100, 111:150), ]
+r = randomForest(Species ~., data=data, importance=TRUE, do.trace=100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
